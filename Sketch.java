c0feeb9739efca441187f7b3dad471e3b34c0297
lucas-pei <lucaspei39@gmail.com>
2022-05-13 12:28:35 -0400

    finished learning objectives

--- Sketch.java
+++ Sketch.java
@@ -2,16 +2,17 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	// global variables
-  float fltPlayerX = 25;
-  float fltPlayerY = 25;
   boolean blnWPressed = false;
   boolean blnAPressed = false;
   boolean blnSPressed = false;
   boolean blnDPressed = false;
-  int intPlayerSpeed = 3;
-  int intCircleSpeed = 5;
   boolean blnDownPressed = false;
   boolean blnUpPressed = false;
+  int intPlayerX = 25;
+  int intPlayerY = 500;
+  int intPlayerSpeed = 3;
+  int intCircleSpeed = 3;
+  int intLives = 3;
 
 
 
@@ -34,11 +35,12 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    // assign random values to the arrays to create coordinates for use in the draw method
     for (int i = 0; i < fltCircleX.length; i++) {
       fltCircleX[i] = random(width);
     }
     for (int i = 0; i < fltCircleY.length; i++) {
-      fltCircleY[i] = random(height);
+      fltCircleY[i] = random(height - 100);
     }
   }
 
@@ -47,47 +49,78 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     background(0);
-    // level 2: draws 25 snowflakes in random locaions, snow falls faster if down arrow is pressed, slower if up arrow is pressed
+    // draws 25 snowflakes travelling down the screen in random locations using fltCircleX and fltCircleY arrays
     for (int i = 0; i < fltCircleY.length; i++) {
-      fill(255);
-      ellipse(fltCircleX[i], fltCircleY[i], 50, 50);
+      // the snowflakes will not draw if ballHideStatus[i] is true
+      if (!ballHideStatus[i]){
+        fill(255);
+        ellipse(fltCircleX[i], fltCircleY[i], 50, 50);
+        fltCircleY[i] += intCircleSpeed;
+        
+        // the snowflakes go back to the top of the screen once they reach the bottom
+        if (fltCircleY[i] > height) {
+          fltCircleY[i] = 0;
+        }
+      }      
+    }
 
-      if (fltCircleY[i] > height) {
-        fltCircleY[i] = 0;
-      }
-      if (keyCode != UP && keyCode != DOWN) {
-        fltCircleY[i] += 3;
-      }
-      else if (keyCode == UP){
-        fltCircleY[i] += 1;
-      }
-      else if (keyCode == DOWN){
-        fltCircleY[i] += 6;
-      }
+    // controls the speed of the snowflakes using arrow keys: up slows, down speeds up
+    if (blnUpPressed && intCircleSpeed > 1) {
+      intCircleSpeed -=1;
+    } 
+    else if (blnDownPressed && intCircleSpeed < 25){
+      intCircleSpeed +=1;
     }
     
-    // level 3: blue player circle with 3 lives, and loses a life every time he collides with a snowflake
+    // draws a blue player circle to the screen, who is controlled using the wasd keys. 
     fill(0, 0, 255);
-    ellipse(fltPlayerX, fltPlayerY, 25, 25);
+    ellipse(intPlayerX, intPlayerY, 25, 25);
     if (blnWPressed){
-      fltPlayerY -= intPlayerSpeed;
+      intPlayerY -= intPlayerSpeed;
     }
     if (blnSPressed){
-      fltPlayerY += intPlayerSpeed;
+      intPlayerY += intPlayerSpeed;
     }
     if (blnAPressed){
-      fltPlayerX -= intPlayerSpeed;
+      intPlayerX -= intPlayerSpeed;
     }
     if (blnDPressed){
-      fltPlayerX += intPlayerSpeed;
+      intPlayerX += intPlayerSpeed;
     }
+
+    // gives the player three lives, and a life is lost every time the player circle touches a snowflake
     for (int i = 0; i < fltCircleY.length; i++){
-      if(dist(fltPlayerX + 12.5f, fltPlayerY + 12.5f, fltCircleX[i], fltCircleY[i]) < 25){
-        ellipse(100, 100, 300, 300);
+      if(dist(intPlayerX, intPlayerY, fltCircleX[i], fltCircleY[i]) < 37.5){
+        fltCircleY[i] = 0;
+        intLives -= 1;
+      }
+    }
+
+    // draws three squares in the top right of the screen to show the user how many lives they have
+    for (int i = 0; i < intLives; i++){
+      fill(255, 0, 0);
+      rect(550 - (i * 50), 30, 30, 30);
+    }
+
+    // if the user has no more lives, a white rectange is drawn to cover the entire screen
+    if (intLives <= 0){
+      fill(255);
+      rect(0, 0, width, height);
+    }
+
+    // if the mouse is pressed within a snowflake, ballHideStatus[i] is changed to true
+    for(int i = 0; i < fltCircleY.length; i++){
+      if(dist(mouseX, mouseY, (fltCircleX[i]), fltCircleY[i]) <= 25){
+        if(mousePressed){
+          ballHideStatus[i] = true;
+        }
       }
     }
   }
 
+  /**
+   * Assigns true values to boolean variables when various keys are pressed to be used in the draw method
+   */
   public void keyPressed() {
     if (keyCode == DOWN) {
       blnDownPressed = true;
@@ -95,7 +128,7 @@ public class Sketch extends PApplet {
     else if (keyCode == UP) {
       blnUpPressed = true;
     }
-    if (key == 'w') {
+    else if (key == 'w') {
       blnWPressed = true;
     }
     else if (key == 's') {
@@ -109,7 +142,16 @@ public class Sketch extends PApplet {
     }
   }
   
+  /**
+   * Assigns false values to boolean variables when various keys are released to be used in the draw method
+   */
   public void keyReleased() {
+    if (keyCode == DOWN) {
+      blnDownPressed = false;
+    }
+    else if (keyCode == UP) {
+      blnUpPressed = false;
+    }
     if (key == 'w') {
       blnWPressed = false;
     }
